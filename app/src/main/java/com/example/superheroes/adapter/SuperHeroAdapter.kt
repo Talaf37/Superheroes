a42e0d021e48ec20889871eaafa43f41fe095ec3
Talaf37 <Talaf.carlos@gmail.com>
2024-02-28 12:01:58 +0100

    Added View Binding to the Adapter of RecyclerView

--- app/src/main/java/com/example/superheroes/adapter/SuperHeroAdapter.kt
+++ app/src/main/java/com/example/superheroes/adapter/SuperHeroAdapter.kt
@@ -1,18 +1,17 @@
 package com.example.superheroes.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.superheroes.R
 import com.example.superheroes.data.Superhero
+import com.example.superheroes.databinding.ItemSuperheroBinding
 
 class SuperheroAdapter(private var items:List<Superhero> = listOf()) : RecyclerView.Adapter<SuperheroViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperheroViewHolder {
-        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.item_superhero, parent, false)
-        return SuperheroViewHolder(view)
+        val binding = ItemSuperheroBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SuperheroViewHolder(binding)
     }
 
     override fun getItemCount(): Int = items.size
@@ -28,14 +27,11 @@ class SuperheroAdapter(private var items:List<Superhero> = listOf()) : RecyclerV
     }
 }
 
-class SuperheroViewHolder(view:View) : RecyclerView.ViewHolder(view) {
 
-    //lateinit var binding:ItemSuperheroBinding
-
-    var nameTextView:TextView = view.findViewById(R.id.nameTextView)
+class SuperheroViewHolder(val binding:ItemSuperheroBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun render(superhero: Superhero) {
-        nameTextView.text = superhero.name
+        binding.nameTextView.text = superhero.name
+        binding.photoImageView.setImageResource(R.drawable.baseline_audio_file_24)
     }
-
 }
